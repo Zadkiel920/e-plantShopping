24b8bdcb83dd609e2a6e8b22a831564c15040bc5
Zadkiel920 <jsalvador.almera@gmail.com>
2025-11-03 09:49:52 -0500

    Task 2: Add reducers to CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find((p) => p.name === item.name);
 
-    
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
+    },
+
+    removeItem: (state, action) => {
+      const itemName = action.payload;
+      state.items = state.items.filter((p) => p.name !== itemName);
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+      const selected = state.items.find((p) => p.name === name);
+
+      if (selected) {
+        selected.quantity = amount;
+      }
     },
   },
 });
